de82df75d24b9574d7f0af3b9956fcf076c9e373
Thiago Batista <rodrigo@visualmanager.com.br>
2026-09-01 12:12:56 -0300

    Adicionando novos eventos da reforma tributário a classe ServicosNFe

--- NFe.Classes/Servicos/Evento/gControleEstoque.cs
+++ NFe.Classes/Servicos/Evento/gControleEstoque.cs
@@ -3,13 +3,14 @@
 namespace NFe.Classes.Servicos.Evento
 {
     /// <summary>
-    /// Informações de quantidade de estoque influenciadas pelo evento 112130.
+    /// Informações de quantidade de estoque influenciadas pelos eventos 112130 e 211124.
     /// </summary>
     public class gControleEstoque
     {
         private decimal _qPerecimento;
-        private decimal _vIBS;
-        private decimal _vCBS;
+        private decimal? _vIBS;
+        private decimal? _vCBS;
+
         /// <summary>
         /// Quantidade objeto de roubo, perda, furto ou perecimento.
         /// </summary>
@@ -28,22 +29,34 @@ namespace NFe.Classes.Servicos.Evento
 
         /// <summary>
         /// Valor do crédito IBS referente às aquisições a ser estornado.
+        /// <para>Obrigatório no evento 112130 e não informado no evento 211124.</para>
         /// </summary>
         [XmlElement("vIBS")]
         public decimal vIBS
         {
-            get { return _vIBS.Arredondar(2); }
+            get { return _vIBS.GetValueOrDefault(); }
             set { _vIBS = value.Arredondar(2); }
         }
 
         /// <summary>
         /// Valor do crédito CBS referente às aquisições a ser estornado.
+        /// <para>Obrigatório no evento 112130 e não informado no evento 211124.</para>
         /// </summary>
         [XmlElement("vCBS")]
         public decimal vCBS
         {
-            get { return _vCBS.Arredondar(2); }
+            get { return _vCBS.GetValueOrDefault(); }
             set { _vCBS = value.Arredondar(2); }
+        }
+
+        public bool ShouldSerializevIBS()
+        {
+            return _vIBS.HasValue;
+        }
+
+        public bool ShouldSerializevCBS()
+        {
+            return _vCBS.HasValue;
         }
     }
 }
